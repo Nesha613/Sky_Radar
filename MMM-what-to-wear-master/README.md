dfddb57edc0923abb777f35ca281301c48c717a3
Tichi Kenani <kenanitichi@gmail.com>
2022-12-02 13:56:26 -0800

    add What-to-wear library

---
+++ MMM-what-to-wear-master/README.md
@@ -0,0 +1,18 @@
+# MMM-what-to-wear
+
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/).
+
+Show icons of clothes to wear, e.g. t-shirt and shorts, depending on the weather
+
+## Using the module
+
+To use this module, add the following configuration block to the modules array in the `config/config.js` file:
+```js
+var config = {
+    modules: [
+        {
+            module: 'MMM-what-to-wear'
+        }
+    ]
+}
+```
---
+++ MMM-what-to-wear-master/node_helper.js
@@ -0,0 +1,22 @@
+/* Magic Mirror
+ * Node Helper: MMM-what-to-wear
+ *
+ * By James Feigel
+ * MIT Licensed.
+ */
+
+var NodeHelper = require("node_helper");
+
+module.exports = NodeHelper.create({
+
+	// Override socketNotificationReceived method.
+
+	/* socketNotificationReceived(notification, payload)
+	 * This method is called when a socket notification arrives.
+	 *
+	 * argument notification string - The identifier of the noitication.
+	 * argument payload mixed - The payload of the notification.
+	 */
+	socketNotificationReceived: function(notification, payload) {
+	}
+});
